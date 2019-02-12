6dbea55f4bcaedd9040f61487207f8688abf9df0
adash333 <adash333@hotmail.com>
2019-02-12 17:20:01 +0900

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  initValue: number;  // 元本入力欄と双方向データバインド
+  rate: number;       // 金利入力欄と双方向データバインド
+ 
+  constructor() {
+  }
+ 
+  // 複利計算
+  calc(): number {
+    if (isNaN(this.initValue) || isNaN(this.rate)) {
+      // 元本または利率が数字でないときはnullを返す
+      return null;
+    }
+    let answer: number = this.initValue;
+    for (let i = 0; i < 10; i++) {
+      // 金利計算を10回繰り返して複利計算
+      answer = answer * (1 + this.rate / 100)
+    }
+    return Math.floor(answer);  // 計算結果を整数に変換
+  }
+
+    // 年毎の金額計算
+    calcArray(): number[] {
+      if (isNaN(this.initValue) || isNaN(this.rate)) {
+      // 元本または利率が数字でないときはnullを返す
+      return null;
+      }
+      let answer: number = this.initValue;
+      let ret: number[] = [answer];
+      for (let i = 0; i < 10; i++) {
+        // 金利計算を10回繰り返して複利計算
+        answer = answer * (1 + this.rate / 100)
+        ret.push(Math.floor(answer)); // 整数に変換
+      }
+      return ret;
+  }
+
 }
